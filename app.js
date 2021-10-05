30cb0a10c9591666315cecd01deadfff547b182d
HorizonPoint528 <91239895+HorizonPoint528@users.noreply.github.com>
2021-10-05 01:36:55 -0400

    added squares to grid

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () =>  {
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.getElementById('score')
+    const resultDisplay = document.getElementById('result')
+    let squares = []
+    const width = 4
+    let score = 0
+  
+    //create the playing board
+    function createBoard() {
+      for (let i=0; i < width*width; i++) {
+        square = document.createElement('div')
+        square.innerHTML = 0
+        gridDisplay.appendChild(square)
+        squares.push(square)
+      }
+    }
+    createBoard()
+
+    
+})
